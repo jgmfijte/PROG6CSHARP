361f7b93fbb27bd55b78a6b04eb48771eb4f224d
jgmfijte <jgmfijte@student.avans.nl>
2014-01-20 11:38:44 +0100

    Algoritme voor buttonClick event. Kijk in de commented stuff voor een gebruiksaanwijzing.

--- MensErgerJeNiet/MensErgerJeNiet/Controllers/SpelController.cs
+++ MensErgerJeNiet/MensErgerJeNiet/Controllers/SpelController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace MensErgerJeNiet
 {
@@ -27,6 +28,31 @@ namespace MensErgerJeNiet
             gameWindow.DobbelValue.Content = DobbelsteenSingleton.Instance.GooiDobbelsteen();
         }
 
+        public void FieldButtonClicked(string buttonName)
+        {
+            string[] splittedUnderscore = buttonName.Split('_');
+            char[] splittedValues = splittedUnderscore[0].ToCharArray();
 
+            string totalString = "";
+            int totalValue;
+
+            for (int c = 1; c < splittedValues.Length; c++)
+            {
+                totalString += splittedValues[c].ToString();
+            }
+
+            try
+            {
+                totalValue = int.Parse(totalString);
+                // in dit try blok de volgende functie aanroepen met de value totalValue. Alle exceptions worden opgevangen, dus moet goedkomen.
+                // aub niet aan de code hierboven zitten.
+
+                Console.WriteLine(totalValue.ToString()); //Deze is voor debug-doeleinden, nog niet verwijderen aub
+            }
+            catch
+            {
+                MessageBox.Show("Helaas is er iets met het bord niet goed, wij willen u verzoeken om dit te melden bij de makers.");
+            }
+        }
     }
 }
